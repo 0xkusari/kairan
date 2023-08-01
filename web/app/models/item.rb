afd27c7f657eac6a7df17e0460efab6b181c9fd4
june29 <june29.jp@gmail.com>
2023-08-01 14:19:07 +0900

    This is symbol

--- web/app/models/item.rb
+++ web/app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
 
   validates :channel_id, presence: true
   validates :title, presence: true
-  validates :url, presence: true, length: { in: 1..8000 }, uniqueness: { scope: channel_id }
+  validates :url, presence: true, length: { in: 1..8000 }, uniqueness: { scope: :channel_id }
 end
