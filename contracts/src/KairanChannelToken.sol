988be6ff8c19577b483dbda19153bf314eb94682
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2023-08-15 12:59:55 +0900

    importを修正

--- contracts/src/KairanChannelToken.sol
+++ contracts/src/KairanChannelToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract KairanChannelToken is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
